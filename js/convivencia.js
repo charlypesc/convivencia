78d33f6c93995a4e2f2e52a5ea1dabab78553f4a
charlypesc <cparedes.escobar@gmail.com>
2020-03-24 23:52:58 -0300

    se agrega en funcion actualiza los parametros para iniciar el setData

--- js/convivencia.js
+++ js/convivencia.js
@@ -36,29 +36,46 @@ function registra(){
 
 
 function actualiza(id){
-  
         
+        // creacion de variables con id distintivos
         var fechaFull   ='fecha'+id;
         var procedimientoFull='procedimiento'+id;
         var intervieneFull='interviene'+id;
         var cargoFull='cargo'+id;
         var registroFull='registro'+id;
-        
+        // se captura la info de cada elemento del informe
         var fecha=document.getElementById(fechaFull).innerText;
         var procedimiento=document.getElementById(procedimientoFull).innerText;
         var interviene=document.getElementById(intervieneFull).innerText;
         var cargo=document.getElementById(cargoFull).value;
-        var registro=document.getElementById(registroFull).innerText;
+        var registro=document.getElementById(registroFull).innerHTML;
+        console.log(registro);
         var proceso="Actualiza";    
+
+        //se trasvasija la info en la vista modal
+
         document.getElementById("form_fecha").value=fecha;      
         document.getElementById("form_proce").value=procedimiento;  
         document.getElementById("inputOption").value=interviene;
         document.getElementById("select_option").value=cargo;
-        document.getElementById("form_registro").value=registro;
+        // x=document.getElementById("form_registro").innerHTML=registro;
         document.getElementById("btn-modal").innerText=("Actualizar");
         document.getElementById("proceso").value=proceso;
         document.getElementById("actualizaId").value=id;
 
+        ClassicEditor
+        .create( document.querySelector( '#form_registro' ),{
+            toolbar:['heading','|','bold','italic','bulletedList','numberedList','blockQuote','undo','redo']
+        } )
+        .then(editor => {
+          console.log(editor);
+          editor.setData(registro); 
+        })
+        .catch( error => {
+            console.error( error );
+        } );
+
+      
       
             $("#form_ficha").submit(function(){
                 var datoForm=$(this).serialize();
