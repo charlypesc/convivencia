a9983840b47c838798bedefb3e07c67c1e5f71ad
charlypesc <cparedes.escobar@gmail.com>
2020-03-26 17:58:34 -0300

    otros

--- print.php
+++ print.php
@@ -2,9 +2,11 @@
 
     require __DIR__.'/vendor/autoload.php';
     use Spipu\Html2Pdf\Html2Pdf;
-
+    ob_start();
+    require_once 'print_view.php';
+    $html = ob_get_clean();
     $html2pdf = new Html2Pdf('P','A4','es','true','UTF-8');
-    $html2pdf->writeHTML('<h1>Hello people</h1>');
+    $html2pdf->writeHTML($html);
     $html2pdf->output('pdf_pdf.pdf');
     
-?>
+?>  
